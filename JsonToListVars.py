d4587e47eb54ba2c2e0a3b6decc416d4df8b0d2f
Bruno Higino de Souza <72587932+BrunoHigino06@users.noreply.github.com>
2022-08-12 08:19:47 -0300

    2022.08.12

--- JsonToListVars.py
+++ JsonToListVars.py
@@ -1,54 +1,12 @@
 import json
-
-jsonfile = """{
-    "Accounts": [
-            {
-                    "Arn": "arn:aws:organizations::111111111111:account/o-exampleorgid/111111111111",
-                    "JoinedMethod": "INVITED",
-                    "JoinedTimestamp": 1481830215.45,
-                    "Id": "111111111111",
-                    "Name": "Master Account",
-                    "Email": "bill@example.com",
-                    "Status": "ACTIVE"
-            },
-            {
-                    "Arn": "arn:aws:organizations::111111111111:account/o-exampleorgid/222222222222",
-                    "JoinedMethod": "INVITED",
-                    "JoinedTimestamp": 1481835741.044,
-                    "Id": "222222222222",
-                    "Name": "Production Account",
-                    "Email": "alice@example.com",
-                    "Status": "ACTIVE"
-            },
-            {
-                    "Arn": "arn:aws:organizations::111111111111:account/o-exampleorgid/333333333333",
-                    "JoinedMethod": "INVITED",
-                    "JoinedTimestamp": 1481835795.536,
-                    "Id": "333333333333",
-                    "Name": "Development Account",
-                    "Email": "juan@example.com",
-                    "Status": "ACTIVE"
-            },
-            {
-                    "Arn": "arn:aws:organizations::111111111111:account/o-exampleorgid/444444444444",
-                    "JoinedMethod": "INVITED",
-                    "JoinedTimestamp": 1481835812.143,
-                    "Id": "444444444444",
-                    "Name": "Test Account",
-                    "Email": "anika@example.com",
-                    "Status": "ACTIVE"
-            }
-    ]
-}"""
-
-
-database = json.loads(jsonfile)
-
-x = 'Account : ' 
-y = 1
-
-for accountid in database['Accounts']:
-        print (x, y)
-        print(accountid['Id'])
-        y = y + 1
-    
+ 
+# Opening JSON file
+with open('data.json') as json_file:
+    data = json.load(json_file)
+ 
+    # Print the type of data variable
+    print("Type:", type(data))
+ 
+    # Print the data of dictionary
+    for accountid in data['Accounts']:
+          print(accountid['Id'])
